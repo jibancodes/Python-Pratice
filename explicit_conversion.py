5465d7f1c35492a8242788768145c53c9e8fd2b2
Jiban Choudhury <choudhuryjiban2009@gmail.com>
2026-09-06 22:09:25 +0530

    add explicit_conversion

---
+++ explicit_conversion.py
@@ -0,0 +1,5 @@
+#Take a number or int as input,convert it to float,print both the original and converted values with their data types.
+number=input("Enter a value:")
+converted_value=float(number)
+print("The original value is:",number,"Data Type:",type(number))
+print("The converted value is:",converted_value,"Data Type:",type(converted_value))
