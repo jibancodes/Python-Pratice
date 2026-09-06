67c30424be4ce1bf94298967d48b33e127ca64f2
Jiban Choudhury <choudhuryjiban2009@gmail.com>
2026-09-06 21:08:18 +0530

    add task06

---
+++ task06.py
@@ -0,0 +1,4 @@
+#Program to take age as input and print value entered and its data types.
+age=int(input("Please enter your age:"))
+print("Your age is",age)
+print("Data type of variable age is",type(age))
