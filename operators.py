471fb46762d2a1bb52be2d3274773745662199f8
Jiban Choudhury <choudhuryjiban2009@gmail.com>
2026-09-06 23:04:20 +0530

    add operators

---
+++ operators.py
@@ -0,0 +1,34 @@
+#Arithmetic Operators
+x=10
+y=5
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+
+#Comparision Operators
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+#Logical Operators
+print("AND operator result:",x>y and x<y)
+print("OR operator result:",x>y or x<y)
+print("NOT operator result:",not(x>y))
+
+#Assignment Operatiors
+a=2
+b=5
+a+=6 #a=a+6
+print(a)
+b-=2 #b=b-2
+print(b)
+a*=3 #a=a*3
+print(a)
+a/=2 #a=a/2
+print(a)
