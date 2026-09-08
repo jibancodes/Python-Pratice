ac4ca05925b551545fc2c6090b61de6668929f5f
Jiban Choudhury <choudhuryjiban2009@gmail.com>
2026-09-08 23:57:30 +0530

    add smart_temp_converter

---
+++ smart_temp_converter.py
@@ -0,0 +1,7 @@
+#Take input in celsius and print its equivalent in fahrenheit and kelvin(Use explicit type converaion and ariyhmetic operations.)
+
+C=float(input("Enter Temprature in Celsius:"))
+F=(C*(9/5))+32
+print("Temprature in Fahrenheit:",F)
+K=C+273.15
+print("Temprature in Kelvin:",K)
