10cc8509e4e7316d9b86a16a36d8fc5037dba333
Jiban Choudhury <choudhuryjiban2009@gmail.com>
2026-09-09 01:13:29 +0530

    add slicing

---
+++ slicing.py
@@ -0,0 +1,9 @@
+#Question on slicing
+#Take input and print middle 3 characters and last 2 characters.
+
+str=input("Enter the value:")
+mid=len(str)//2
+output1=str[mid-1:mid+2]
+print(output1)
+output2=str[-2:]
+print(output2)
