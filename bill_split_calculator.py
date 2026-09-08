af3f4ff0ccf0021a60059a6a6c710cca53711f67
Jiban Choudhury <choudhuryjiban2009@gmail.com>
2026-09-09 00:11:11 +0530

    add bill_split_calculator

---
+++ bill_split_calculator.py
@@ -0,0 +1,6 @@
+#Write a program that takes total bill amount and number of friends as input.Calculate how much each person will pay. Also print the data type of each variable used.
+
+x=float(input("Enter Total bill amount:"))
+y=int(input("Total number of friends:"))
+z=x/y
+print("Each will pay:",z)
