6eab6c2f8c9afdb4e13f39547bc097e3e04f124e
Jiban Choudhury <choudhuryjiban2009@gmail.com>
2026-09-10 00:56:00 +0530

    add ptatice01

---
+++ pratice1.py
@@ -0,0 +1,12 @@
+#Write a program that takes a number as input and prints:
+#"positive" if number>0
+#"zero" if number==0
+#"negative" if number<0
+
+num=int(input("Enter a Number:"))
+if (num>0):
+    print("This is a positive number.")
+elif (num==0):
+            print("Zero")
+else:
+                        print("This is a negative number.")
