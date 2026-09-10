ddc0c1e20e80cd89f9b2bcba87b54083800fabfd
Jiban Choudhury <choudhuryjiban2009@gmail.com>
2026-09-11 00:46:26 +0530

    add list_method

---
+++ list_method.py
@@ -0,0 +1,17 @@
+#List method in python.
+
+foods=["samosa","pizza","burger"]
+print(foods[0])
+print(foods[2])
+print(len(foods))
+print("Second value of the list:",foods[1])
+foods[0]="gulabjamun"
+print(foods)
+print(foods[0:2])
+print(foods[:2])
+print(foods[-3:-1])
+
+marks=[35,72,89,96,88,46]
+print(len(marks))
+print(max(marks))
+print(min(marks))
