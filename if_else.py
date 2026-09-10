7b993c6e187fd3081335bb11b83e4e8679dba176
Jiban Choudhury <choudhuryjiban2009@gmail.com>
2026-09-11 00:11:51 +0530

    add if_else

---
+++ if_else.py
@@ -0,0 +1,9 @@
+#Program to takes a number as input and prints: "positive "    if the number >0 , "Zero" if the number== 0 , "Negative " if the number<0.
+
+num= int(input("Enter a number:"))
+if (num>0):
+    print("Positive")
+elif (num==0):
+            print("Zero")
+else:
+print("Negative")
